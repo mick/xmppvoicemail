126c437cca8271dc17ce190fd9829c9c283b58b0
Mick Thompson <mick@collecta.com>
2010-11-14 22:46:30 -0800

    add jid option comment

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Author: Mick Thompson (dthompson@gmail.com)
 #
 import os
 from google.appengine.ext import webapp
@@ -22,7 +10,9 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import xmpp_handlers
 from google.appengine.api import xmpp
 
-USERJID = "mick@collecta.com"
+
+# change this to the JID of your Xmpp account (example: dthompson@gmail.com)
+USERJID = "you@domain.com"
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
